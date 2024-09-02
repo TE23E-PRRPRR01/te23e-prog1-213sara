0557e8415158ab4d3c0373851a009fac5ea7ad0d
Sara Kerri <sara.kerri@elev.ga.ntig.se>
2024-09-02 14:51:39 +0200

    Träna på if else satser

---
+++ Kapitel-3/EngChart/Program.cs
@@ -0,0 +1,61 @@
+﻿// Program som simulerar kända Engineering FlowChart
+Console.Clear();
+Console.WriteLine("Välkommen till ingenjörens flödesschema");
+
+// Visa en ASCii art
+Console.WriteLine($"""
+                      -   ,--_--.
+               -           \      `.
+                      -     "-_ _   \
+     -                         / F   )
+                   -     -    / / `--'
+              -              / /
+                   -        / /
+            -            __/ /
+                        /,-pJ
+           -        _---L ||
+                  ,"      "//
+     -           /  ,-waw.//\
+                /  /     // J____
+               J  /     // L/----\
+   .           F J     //__//^---'
+     `     ___J  F    '----| |
+  `       J---|  F         F F
+`   `. `   `--J  L        J  F
+    .   .`     L J       J  F
+       .  .    J  \    ,"  F
+         .  `.` \  "--"  ,"
+    hs      ` ``."-____-"
+""");
+
+Console.Write("Does it move? (y/n)");
+string answer = Console.ReadLine();
+
+// Kolla om svar är ja eller nej
+if (answer == "y")
+{ // y = yes
+    Console.Write("Should it? (y/n)");
+    answer = Console.ReadLine();
+    if (answer == "y")
+    {
+        Console.WriteLine("Plz use WD-40!")
+    }
+    else
+    {
+
+    }
+}
+else
+{ // n = no
+    Console.Write("Should it? (y/n)");
+    answer = Console.ReadLine();
+if (answer == "y")
+    {
+        Console.WriteLine("No pronlem!");
+    }
+    else
+    {
+        Console.WriteLine("Plz use duct tape!")
+    }
+
+}
---
+++ Kapitel-3/InteractiveStory/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Formats.Asn1;
+
+Console.WriteLine("Du står mitt i Odenplan, ingen person i sikt.);
+Console.WriteLine( Du behöver ta reda på vad som har skett. Går du hemmåt eller till skola? (hem/skola);
+answer = Console.ReadLine();
+if (answer == hem))
+{
+    Console.WriteLine("Du går hem.");
+    Console.WriteLine("Påväg hem fanns ingen omkring dig, inte ens på tåget.");
+    Console.WriteLine("Hemma var det tomt. Du ropar, "Mamma!"..."Pappa!"... inga svar.);
+    Console.WriteLine("Du går mot ditt sovrum och finner en lapp. Lapp: "Gå mot biblioteket så ser du var resan för dig")
+    Console.WriteLine("Du känner dig skeptisk. Stannar du hemma eller går du mot biblioteket? (stannar hemma/blioteket)");
+
+}
+else
+{
+    
+}
