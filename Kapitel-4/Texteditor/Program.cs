8965b5867eb060ec68f8dfb39deaec28bf29b3ab
Sara Kerri <sara.kerri@elev.ga.ntig.se>
2024-09-23 14:49:03 +0200

    tränat på while-loop och filhantering

---
+++ Kapitel-4/Texteditor/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Clear();
+
+// Litet meny program
+Console.WriteLine("");
+
+// Variabel för valet
+string val = "";
+
+// Programloop som snurrar tills användaren säger avsluta
+while (true)
+{
+    // Visar en meny och läser användarens val
+    Console.Write("""
+    1. Tid
+    2. Datum
+    3. Avsluta
+    Välj ett av alternativen ovan: 
+    """);
+
+    val = Console.ReadLine();
+
+    // Hantera användarens val
+    if (val == "1")
+    {
+
+        Console.WriteLine("Tid:" + DateTime.Nowstring("hh:mm:ss"));
+
+
+    }
+    else if (val == "2")
+    {
+        Console.WriteLine("Datum:" + DateTime.Nowstring("yy:MM:dd"));
+
+    else if (val == "3")
+        {
+            Console.WriteLine("Avslutar...");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Du valde inget giltigt alternativ, försök igen!");
+        }
+
+    }
+}
