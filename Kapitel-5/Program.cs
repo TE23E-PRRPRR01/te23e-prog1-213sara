349cbf75c3b78e414b4bed232b5e51742268429e
Sara Kerri <sara.kerri@elev.ga.ntig.se>
2024-10-16 14:46:33 +0200

    sos

---
+++ Kapitel-5/Program.cs
@@ -0,0 +1,44 @@
+﻿// En liten morsekod app
+Console.Clear();
+Console.WriteLine("En liten morsekod app");
+Console.InputEncoding = System.Text.Encoding.Unicode;
+
+// Två listor
+List<string> alfabetet = [  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+                            "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Å", "Ä",
+                            "Ö", " "];
+
+List<string> morsekod = [   ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+                            "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+                            "..-", "...-", ".--", "-..-", "-.--", "--..", ".--.-", ".-.-", "---.", "/" ];
+
+// Läs in ett meddelande
+Console.Write("Ange ett meddelande");
+string meddelande = Console.ReadLine();
+
+// Plocka ut morsetecknet för detta index
+string morsetecken = morsekod[index]
+Console.WriteLine($"{bokstav} är {morsetecknet}");
+
+// Gå igenom meddelandet bokstav för bokstav (loop)
+foreach (char bokstav in meddelande)
+{
+    // Uppslag i alfaetet efter index
+    int index = alfabetet.IndexOf(bokstav.ToString());
+
+    // Hittar morsetecken (A-Ö)?
+    if (index >= 0)
+    {
+        Console.WriteLine($"{bokstav} finns på index {index}");
+
+        // Plocka ut morsetecken för detta index
+        string morsetecken = morsekod[index];
+        // Console.WriteLine($"{morsetecken}");
+
+    }
+    else
+    {
+        Console.WriteLine("?");
+    }
+
+}
