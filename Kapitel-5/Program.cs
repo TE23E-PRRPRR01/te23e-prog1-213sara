fe1785418c2ada1eee2d1726159f49d1f98ae456
Sara Kerri <sara.kerri@elev.ga.ntig.se>
2024-11-13 15:01:07 +0100

    tränat på if satser

---
+++ Kapitel-5/Program.cs
@@ -0,0 +1,58 @@
+﻿// En liten morsekod app
+using System.Runtime.InteropServices;
+
+Console.Clear();
+Console.WriteLine("En liten morsekod app");
+Console.InputEncoding = System.Text.Encoding.Unicode;
+
+// Två listor
+List<string> alfabetet = [  "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+                            "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Å", "Ä",
+                            "Ö", " "];
+
+List<string> morsekod = [   ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+                            "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+                            "..-", "...-", ".--", "-..-", "-.--", "--..", ".--.-", ".-.-", "---.", "/" ];
+
+// Läs in ett meddelande
+Console.Write("Ange ett meddelande");
+string meddelande = Console.ReadLine();
+
+// Plocka ut morsetecknet för detta index
+string morsetecken = morsekod[index]
+Console.WriteLine($"{bokstav} är {morsetecknet}");
+
+// Gå igenom meddelandet bokstav för bokstav (loop)
+foreach (char bokstav in meddelande)
+{
+    // Uppslag i alfaetet efter index
+    int index = alfabetet.IndexOf(bokstav.ToString());
+
+    // Hittar morsetecken (A-Ö)?
+    if (index >= 0)
+    {
+        Console.WriteLine($"{bokstav} finns på index {index}");
+
+        // Plocka ut morsetecken för detta index
+        string morsetecken = morsekod[index];
+        // Console.WriteLine($"{morsetecken}");
+
+        if (PosixSignal == '.') // '.'
+        {
+            // 100Hz, 200ms
+            Console.Beep(1000, 200);
+        }
+        else // '-'
+        { 
+            Console.Beep(1000, 300);
+        }
+
+        // paus i koden
+        Thread.Sleep(100);
+    }
+    else
+    {
+        Console.WriteLine("?");
+    }
+
+}
---
+++ Kapitel-6/Dungeon/Program.cs
@@ -0,0 +1,56 @@
+﻿// Dungeon spel med massor av rum
+using Microsoft.VisualBasic;
+
+Console.Clear();
+Console.WriteLine("Ett Dungeon-spel");
+
+// Programvariabler (state variables)
+string rum = "hallen";
+List<string> inventarie = [];
+
+// Spelloop
+while (true)
+{
+    // Är vi i hallen?
+   if (rum == "hallen")
+   {
+        Console.WriteLine("Du är i hallen");
+        Console.WriteLine("1. Titta runt");
+        Console.WriteLine("2. Gå rill nästa rum");
+        Console.Write("Vad vill du göra? ");
+        string val = Console.ReadLine();
+
+    if(val == "1")
+    {
+        Console.WriteLine("Du ser nyckeln på golvet");
+        Console.Write("Vill du plocka upp nyckeln?");
+        val == Console.ReadLine().ToLower();
+
+        if (val == "j")
+        {
+            inventaire.Add("Nyckel");
+            Console.WriteLine("Du plockade bort nyckeln");
+        }
+        else
+        {
+            Console.WriteLine("Du lämnar nyckeln på golvet!");
+        }
+
+    }
+   
+   else if(val == "2")
+   {
+     rum = "köket"
+     Console.WriteLine("...du går in i köket...");
+     Console.WriteLine("I köket ser du en lapp");
+     Console.WriteLine("Vill du läsa lappen?(J/N)");
+    val == Console.ReadLine().ToLower();
+
+   }
+    else if(val == "Ja")
+    {
+        Console.WriteLine("På lappen står det "RYM GENAST FRÅN HUSET OCH GÅ INTE NED I KÄLLAREN!!"");
+
+    }
+   }
+}
