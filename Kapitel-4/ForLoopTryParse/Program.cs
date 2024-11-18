30926c263b6524200b90688a0b68643609de5ae1
Sara Kerri <sara.kerri@elev.ga.ntig.se>
2024-11-18 14:44:17 +0100

    TryParse

---
+++ Kapitel-4/ForLoopTryParse/Program.cs
@@ -0,0 +1,13 @@
+﻿// Loop mätare
+Console.Clear();
+Console.WriteLine("Räkna....");
+
+//namn
+Console.WriteLine("Vad heter du?");
+string namn = Console.ReadLine();
+
+// upprepa namn
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Antal: {i + ". " + namn}" );
+}
