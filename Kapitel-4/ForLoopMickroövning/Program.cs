8ce1879713cd8b39afe26ff61e84ef868e2e4738
Sara Kerri <sara.kerri@elev.ga.ntig.se>
2024-11-20 15:00:20 +0100

    tränat på forloop till bakgrundsmusik

---
+++ Kapitel-4/ForLoopMickroövning/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Runtime.ConstrainedExecution;
+using Microsoft.VisualBasic;
+
+Console.Clear();
+
+
+Console.Write("Ange ett startvärde ");
+int startvärde = int.Parse(Console.ReadLine());
+
+Console.Write("Ange ett slutvärde ");
+int slutvärde = int.Parse(Console.ReadLine());
+
+int summa = 0;
+
+for (var i = startvärde; i < slutvärde + 1; i++)
+{
+    summa += i;
+}
+    Console.WriteLine($"Summa: {summa}");
+
+// två separata for loopar
+Console.WriteLine("Alla jämna tal 1-20: ");
+for (var i = startvärde; i < slutvärde; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i);
+    }
+    
+}
+
+Console.WriteLine("Alla udda tal 1-20: ");
+for (var i = startvärde; i < slutvärde; i++)
+{
+     if (i % 2 != 0)
+    {
+        Console.Write(i);
+    }
+}
