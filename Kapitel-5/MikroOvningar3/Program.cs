91f07a5a00edcac6b250f437547b28e3bb10e9d3
Sara Kerri <sara.kerri@elev.ga.ntig.se>
2024-12-02 14:52:08 +0100

    fortsatte att träna på listor

---
+++ Kapitel-5/MikroOvningar3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Listor 3
+Console.Clear();
+Console.WriteLine("Listor 3");
+
+// 1. Mat lista
+List<string> listaMat = ["Pizza", "Hamburgare", "Sushi"];
+foreach (string mat in listaMat)
+{
+    Console.WriteLine($"-{mat}");
+}
+
+// 2. Lista år
+List<int> listaÅr = [1492, 1776, 1914, 1945, 1969];
+foreach (int år in listaÅr)
+{
+    Console.WriteLine($"-{år}");
+     
+        // 3. Utskrivet på en rad
+        Console.Write($"- {år}");
+}
+
+// 4. Låt användaren skriva in namn
+Console.WriteLine("Ange ett namn att lägga till: ");
+string svar = Console.ReadLine();
+
+if (svar == "exit")
+{
+    Console.WriteLine("Tack att du körde namn tilläggaren");
+}
+else
+{
+    
+}
+
+// 5.
+// 6.
+// 7.
+// 8.
+// 9.
+// 10.
+// 11.
+// 12.
+// 13.
