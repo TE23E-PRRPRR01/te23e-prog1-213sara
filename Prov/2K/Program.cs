311d1000f5b9f924aa8d76d139a75330f2a49de2
Sara Kerri <sara.kerri@elev.ga.ntig.se>
2024-12-11 15:03:52 +0100

    tränade på övningsprovet

---
+++ Prov/2K/Program.cs
@@ -0,0 +1,81 @@
+﻿Console.Clear();
+Console.WriteLine("Filmregistret");
+
+// Lista för att lagra filmer
+List<string> listaFilmer = [];
+
+// Be användaren ange ett antal
+Console.Write("Ange antal filmer per sida (3-5): ");
+int antal = int.Parse(Console.ReadLine());
+string antalText = Console.ReadLine();
+int antal = 0;
+while (true)
+{
+    bool success = int.TryParse(antalText, out antal);
+    if (success)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Fel! Var god mata in ett heltal.");
+    }
+}
+
+// Programloop
+while (true)
+{
+    // Visa en meny
+    Console.Write("""
+    1. Lägg till filmer
+    2. Lista alla filmer
+    3. Sök en film
+    4. Avsluta
+
+    Välj ett alternativ: 
+    """);
+    string val = Console.ReadLine();
+
+    // Hanterar med valet
+   if (val == "1")
+   {
+        // Lägg till filmer
+        for (var i = 0; i < antal; i++)
+        {
+        Console.Write("Ange en film att läggs till: ");
+        string film = Console.ReadLine();
+        listaFilmer.Add(film);
+        }
+   }
+   else if(val == "2")
+   {
+        // Skriv ut alla filmer
+        foreach (var filmnamn in listaFilmer)
+        {
+            Console.WriteLine(filmnamn);
+        }
+   }
+   else if(val == "3")
+   {
+        // Sök en film
+        Console.Write("Ange en film att söka efter (första bokstaven): ");
+        string bokstav = Console.ReadLine();
+        foreach (var filmnamn in listaFilmer)
+        {
+            // Kolla om varje film börjar på bokstaven
+            if (filmnamn.StartsWith(bokstav))
+            {
+                Console.WriteLine(filmnamn);
+            }
+        }
+   }
+   else if(val == "4")
+   {
+    Console.WriteLine("Tack för idag!");
+    break;
+   }
+   else
+   {
+    Console.WriteLine("Fel val. Försök igen!");
+   }
+}
